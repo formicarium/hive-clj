f50749f923814523ffc7d5aa433cc4d48e41cf58
Leonardo Iacovini <leoiacovini@gmail.com>
2018-02-23 23:11:25 -0300

    Improve ZMQ client interface

--- src/hive_clj/zmq.clj
+++ src/hive_clj/zmq.clj
@@ -1,6 +1,7 @@
 (ns hive-clj.zmq
   (:require [zeromq.zmq :as zmq]
             [cheshire.core :as cheshire]
+            [com.stuartsierra.component :as component]
             [clojure.core.async :as async]))
 
 (def context (zmq/context 1))
@@ -24,27 +25,41 @@
             (recur xs))
         (zmq/send socket (str->bytes x))))))
 
-(defn send-message! [dealer message-map]
+(defn send-dealer-message! [dealer message-map]
   (send! dealer (cheshire/generate-string (:meta message-map)) (cheshire/generate-string (:payload message-map))))
 
 (defn send-channel [dealer]
   (let [ch (async/chan 1000)]
     (async/go-loop []
       (when-let [value (async/<! ch)]
-        (some->> value (send-message! dealer))
+        (some->> value (send-dealer-message! dealer))
         (recur)))
     ch))
-
-(defn new-hive-client! [endpoint ident]
-  (let [dealer (new-dealer-socket! endpoint ident)]
-    {:ident         ident
-     :hive-endpoint endpoint
-     :dealer        dealer
-     :channel       (send-channel dealer)}))
 
 (defn terminate-sender-channel! [ch] (async/close! ch))
 (defn terminate-dealer-socket! [dealer] (zmq/close dealer))
 
+(defprotocol ZMQDealer
+  (send-message! [this message-map]))
+
+(defrecord HiveClient [endpoint ident]
+  component/Lifecycle
+  (start [this]
+    (let [dealer (new-dealer-socket! endpoint ident)]
+      (assoc this :dealer dealer
+                  :channel (send-channel dealer))))
+
+  (stop [this]
+    (terminate-sender-channel! (:channel this))
+    (terminate-dealer-socket! (:dealer this))
+    (dissoc this :channel :dealer))
+
+  ZMQDealer
+  (send-message! [this message-map]
+    (send-dealer-message! (:dealer this) message-map)))
+
+(defn new-hive-client! [endpoint ident]
+  (component/start (->HiveClient endpoint ident)))
+
 (defn terminate-client! [client]
-  (terminate-sender-channel! (:channel client))
-  (terminate-dealer-socket!  (:dealer client)))
+  (component/stop client))
